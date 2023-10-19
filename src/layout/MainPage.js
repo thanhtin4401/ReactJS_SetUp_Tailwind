7bc8924a4815e40a3d0a921cb130251b2a2575bd
thanhtin4401 <thanhtin4401.ufm@gmail.com>
2023-10-19 23:23:06 +0700

    firt commit

---
+++ src/layout/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ExamplePage from "../pages/ExamplePage/ExamplePage";
+
+const MainPage = () => {
+  return (
+    <div>
+      <ExamplePage />
+    </div>
+  );
+};
+
+export default MainPage;
